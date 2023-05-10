20847ea624d0be8a82ff67bf8291847d86a6ed13
andronachi09 <tudor.andronachi09@gmail.com>
2023-05-10 17:57:40 +0200

    first commit

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,21 @@
+import './category-item.style.scss';
+
+const CategoryItem = ({ vasea }) => {
+    const { imageUrl, title } = vasea;
+
+    return(
+        <div className="category-container">
+            <div 
+                className="background-image" 
+                style={{
+                backgroundImage: `url(${imageUrl})`
+                }}/>
+            <div className="category-body-container">
+                <h2>{title}</h2>
+                <p>Shop now</p>
+         </div>
+       </div>
+    )
+};
+
+export default CategoryItem;
---
+++ src/directory/directory.component.jsx
@@ -0,0 +1,14 @@
+import CategoryItem from "../components/category-item/category-item.component";
+import "./directory.styles.scss";
+
+const Directory = ({gheorghe}) => {
+    return(
+        <div className="directory-container">
+            {gheorghe.map((category) => (
+                <CategoryItem key={category.id} vasea={category} />
+            ))}
+        </div>
+    )
+}
+
+export default Directory;
