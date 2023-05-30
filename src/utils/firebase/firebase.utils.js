d9ef1361e18cab033928efa9d5f02af476debbb7
andronachi09 <tudor.andronachi09@gmail.com>
2023-05-30 19:19:09 +0200

    adding new user to Firestore Database

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,58 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDglUClXIr1B6m8xq2fuxqRTJFBnq8_Hz0",
+  authDomain: "online-shop-db2.firebaseapp.com",
+  projectId: "online-shop-db2",
+  storageBucket: "online-shop-db2.appspot.com",
+  messagingSenderId: "8869888977",
+  appId: "1:8869888977:web:2cb098fb6b7f62a98656f0",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+// Setting up user database
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  // getDoc() - get data related to a document
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists()); //check if the user snapshot exists in database, in our case is false
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("Error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
