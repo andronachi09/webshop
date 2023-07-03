1a91f6a142d17ac49332b0d2c4e279b7eb6f151d
andronachi09 <tudor.andronachi09@gmail.com>
2023-07-03 20:01:19 +0200

    chore: updating routes

--- src/App.js
+++ src/App.js
@@ -3,23 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
-
-const Shop = () => {
-  return(
-    <h1>Shop pulica</h1>
-  )
-}
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
-  return(
+  return (
     <Routes>
-      <Route path='/' element={<Navigation />}>
+      <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
-  ) 
+  );
 };
- 
+
 export default App;
