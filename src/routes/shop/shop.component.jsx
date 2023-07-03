3d8a809f2fb194aee3c198dd6fb7e70d390c26a4
andronachi09 <tudor.andronachi09@gmail.com>
2023-07-03 20:00:17 +0200

    feat: shop component

---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SHOP_DATA from "../../shop-data.json";
+
+const Shop = () => {
+  return (
+    <div>
+      {SHOP_DATA.map(({ id, name }) => (
+        <h1 key={id}>{name}</h1>
+      ))}
+    </div>
+  );
+};
+
+export default Shop;
