4be1ffbd90ab0dd170905162a3373bcd58f3f30e
andronachi09 <tudor.andronachi09@gmail.com>
2023-07-03 20:02:40 +0200

    chore: sign in form

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -25,19 +25,15 @@ const SignUpForm = () => {
   };
 
   const signInWithGoogle = async () => {
-    console.log("loshara");
     const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const response = await signInAuthUserWithEmailAndPassword(
-        email,
-        password
-      );
-      console.log(response);
+      await signInAuthUserWithEmailAndPassword(email, password);
       resetFormFields();
     } catch (error) {
       switch (error.code) {
